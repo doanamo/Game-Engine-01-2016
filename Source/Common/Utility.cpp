9da39fbc86145df893b3808cb62686ac66143b07
Piotr Doan <doanpiotr@gmail.com>
2016-01-06 14:52:04 +0100

    Import few utility functions.

--- Source/Common/Utility.cpp
+++ Source/Common/Utility.cpp
@@ -1,6 +1,20 @@
 #include "Precompiled.hpp"
 #include "Utility.hpp"
 
+std::string Utility::GetFileExtension(std::string filename)
+{
+    std::string extension;
+
+    size_t it = filename.rfind('.');
+    if(it != std::string::npos)
+    {
+        extension = filename.substr(it + 1);
+    }
+
+    return extension;
+}
+
+
 std::string Utility::GetTextFileContent(std::string filename)
 {
     std::ifstream file(filename);
--- Source/Common/Utility.hpp
+++ Source/Common/Utility.hpp
@@ -8,6 +8,30 @@
 
 namespace Utility
 {
+    // Clamps a value between the lower and upper range.
+    template<typename Type>
+    Type Clamp(const Type& value, const Type& lower, const Type& upper)
+    {
+        return std::max(lower, std::min(value, upper));
+    }
+
+    // Gets the size of a static array.
+    template<typename Type, size_t Size>
+    size_t ArraySize(const Type(&)[Size])
+    {
+        return Size;
+    }
+
+    // Frees the container's memory.
+    template<typename Type>
+    void ClearContainer(Type& container)
+    {
+        container.swap(Type());
+    }
+
+    // Gets the extension of a file.
+    std::string GetFileExtension(std::string filename);
+
     // Gets the content of a text file.
     std::string GetTextFileContent(std::string filename);
 
