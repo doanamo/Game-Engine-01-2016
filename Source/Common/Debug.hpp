4d8d5cc0c749b540eb30704e8859e4607a62a6d9
Piotr Doan <doanpiotr@gmail.com>
2016-02-28 13:10:37 +0100

    Add custom verify and assert macros.

--- Source/Common/Debug.hpp
+++ Source/Common/Debug.hpp
@@ -31,3 +31,98 @@ namespace Debug
         #endif
     }
 }
+
+//
+// Debug Macros
+//
+
+#define DEBUG_EXPAND_MACRO(x) x
+
+#define DEBUG_PRINT_ASSERT_SIMPLE(type, expression) \
+    Logger::ScopedMessage(Logger::GetGlobal()).SetSource(__FILE__).SetLine(__LINE__) \
+        << type << ": \"" << expression << "\"";
+
+#define DBEUG_PRINT_MESSAGE(type, expression, message) \
+    Logger::ScopedMessage(Logger::GetGlobal()).SetSource(__FILE__).SetLine(__LINE__) \
+        << type << ": \"" << expression << "\" - " << message;
+
+//
+// Verify Macro
+//  Makes sure that a given expression is true.
+//
+//  Behaviour in different build types:
+//  - Debug: Triggers a breakpoint
+//  - Release: Triggers a breakpoint
+//
+
+#define VERIFY_STRING "Assertion failed"
+
+#ifndef NDEBUG
+    #define VERIFY_SIMPLE(expression)                                           \
+        if(!(expression))                                                       \
+        {                                                                       \
+            DEBUG_PRINT_SIMPLE(VERIFY_STRING, #expression);                     \
+            __debugbreak();                                                     \
+        }
+
+    #define VERIFY_MESSAGE(expression, message)                                 \
+        if(!(expression))                                                       \
+        { \                                                                     \
+            DBEUG_PRINT_MESSAGE(VERIFY_STRING, #expression, message);           \
+            __debugbreak();                                                     \
+        }
+#else
+    #define VERIFY_SIMPLE(expression)                                           \
+        if(!(expression))                                                       \
+        {                                                                       \
+            DEBUG_PRINT_SIMPLE(VERIFY_STRING, #expression);                     \
+            __debugbreak();                                                     \
+        }
+
+    #define VERIFY_MESSAGE(expression, message)                                 \
+        if(!(expression))                                                       \
+        {                                                                       \
+            DBEUG_PRINT_MESSAGE(VERIFY_STRING, #expression, message);           \
+            __debugbreak();                                                     \
+        }
+#endif
+
+#define VERIFY_DEDUCE(arg1, arg2, arg3, ...) arg3
+#define VERIFY_CHOOSER(...) DEBUG_EXPAND_MACRO(VERIFY_DEDUCE(__VA_ARGS__, VERIFY_MESSAGE, VERIFY_SIMPLE))
+
+#define VERIFY(...) DEBUG_EXPAND_MACRO(VERIFY_CHOOSER(__VA_ARGS__)(__VA_ARGS__))
+
+//
+// Assert Macro
+//  Makes sure that a given expression is true.
+//
+//  Behaviour in different build types:
+//  - Debug: Triggers a breakpoint
+//  - Release: Check is stripped
+//
+
+#define ASSERT_STRING "Assertion failed"
+
+#ifndef NDEBUG
+    #define ASSERT_SIMPLE(expression)                                           \
+        if(!(expression))                                                       \
+        {                                                                       \
+            DEBUG_PRINT_SIMPLE(ASSERT_STRING, #expression);                     \
+            __debugbreak();                                                     \
+        }
+
+    #define ASSERT_MESSAGE(expression, message)                                 \
+        if(!(expression))                                                       \
+        {                                                                       \
+            DBEUG_PRINT_MESSAGE(ASSERT_STRING, #expression, message);           \
+            __debugbreak();                                                     \
+        }
+#else
+    #define ASSERT_SIMPLE(expression) ((void)0)
+    #define ASSERT_MESSAGE(expression, message) ((void)0) 
+#endif
+
+#define ASSERT_DEDUCE(arg1, arg2, arg3, ...) arg3
+#define ASSERT_CHOOSER(...) DEBUG_EXPAND_MACRO(ASSERT_DEDUCE(__VA_ARGS__, ASSERT_MESSAGE, ASSERT_SIMPLE))
+
+#define ASSERT(...) DEBUG_EXPAND_MACRO(ASSERT_CHOOSER(__VA_ARGS__)(__VA_ARGS__))
